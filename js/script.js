11a5022322fbc838eb60c8ac0c203f2752a1a04a
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-11-20 20:57:40 -0600

    Manejo de Variables en JavaScript

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let nombre = "Marco";
+const estatura = 1.82;
+let edad = 47;
+const factorPerruno = 7;
+let edadPerruna = edad/factorPerruno;
+var yanoseusa = "Rafael";
+
+console.log(nombre + " tiene " + Math.ceil(edadPerruna) + " años perrunos");
+console.log(`${nombre} tiene ${Math.ceil(edadPerruna)} años perrunos`);
+//var no es recomendable hoy en dia.
+
+  
+// console.log(estatura);
+// console.log(nombre);
+
+// nombre = "Daniel";
+// console.log(nombre);
+
+ 
